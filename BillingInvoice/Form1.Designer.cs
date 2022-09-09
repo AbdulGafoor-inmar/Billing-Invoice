582d9150b80aa49aaa1e3de12a05894c9898fe3a
AbdulGafoor-inmar <111868448+AbdulGafoor-inmar@users.noreply.github.com>
2022-09-09 10:01:11 +0530

    Add files via upload

---
+++ BillingInvoice/Form1.Designer.cs
@@ -0,0 +1,417 @@
+﻿
+namespace BillingInvoice
+{
+    partial class BillingSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbItems = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioGents = new System.Windows.Forms.RadioButton();
+            this.radioLadies = new System.Windows.Forms.RadioButton();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnAddtocart = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSub = new System.Windows.Forms.TextBox();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNet = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPaid = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBalance = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1169, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(167, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(642, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BILLING MANAGEMENT SYSTEM";
+            // 
+            // cmbItems
+            // 
+            this.cmbItems.FormattingEnabled = true;
+            this.cmbItems.Location = new System.Drawing.Point(240, 189);
+            this.cmbItems.Name = "cmbItems";
+            this.cmbItems.Size = new System.Drawing.Size(714, 28);
+            this.cmbItems.TabIndex = 1;
+            this.cmbItems.SelectedIndexChanged += new System.EventHandler(this.cmbItems_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(58, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Item:-";
+            // 
+            // radioGents
+            // 
+            this.radioGents.AutoSize = true;
+            this.radioGents.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.radioGents.Location = new System.Drawing.Point(370, 135);
+            this.radioGents.Name = "radioGents";
+            this.radioGents.Size = new System.Drawing.Size(157, 26);
+            this.radioGents.TabIndex = 3;
+            this.radioGents.TabStop = true;
+            this.radioGents.Text = "GENTS WEAR";
+            this.radioGents.UseVisualStyleBackColor = true;
+            this.radioGents.CheckedChanged += new System.EventHandler(this.radioGents_CheckedChanged);
+            // 
+            // radioLadies
+            // 
+            this.radioLadies.AutoSize = true;
+            this.radioLadies.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.radioLadies.Location = new System.Drawing.Point(630, 135);
+            this.radioLadies.Name = "radioLadies";
+            this.radioLadies.Size = new System.Drawing.Size(157, 26);
+            this.radioLadies.TabIndex = 4;
+            this.radioLadies.TabStop = true;
+            this.radioLadies.Text = "LADIES WEAR";
+            this.radioLadies.UseVisualStyleBackColor = true;
+            this.radioLadies.CheckedChanged += new System.EventHandler(this.radioLadies_CheckedChanged);
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(63, 270);
+            this.txtPrice.Multiline = true;
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 38);
+            this.txtPrice.TabIndex = 5;
+            this.txtPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(78, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Price:-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(242, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Quantity:-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(419, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total:-";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(404, 270);
+            this.txtTotal.Multiline = true;
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 38);
+            this.txtTotal.TabIndex = 9;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAddtocart
+            // 
+            this.btnAddtocart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnAddtocart.Location = new System.Drawing.Point(535, 248);
+            this.btnAddtocart.Name = "btnAddtocart";
+            this.btnAddtocart.Size = new System.Drawing.Size(157, 64);
+            this.btnAddtocart.TabIndex = 11;
+            this.btnAddtocart.Text = "Add To Cart";
+            this.btnAddtocart.UseVisualStyleBackColor = true;
+            this.btnAddtocart.Click += new System.EventHandler(this.btnAddtocart_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(28, 318);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(926, 256);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Item No.";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Product";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Price";
+            this.columnHeader3.Width = 200;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Quantity";
+            this.columnHeader4.Width = 200;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Amount";
+            this.columnHeader5.Width = 200;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label6.Location = new System.Drawing.Point(28, 598);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 22);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Sub Total:-";
+            // 
+            // txtSub
+            // 
+            this.txtSub.Location = new System.Drawing.Point(120, 595);
+            this.txtSub.Name = "txtSub";
+            this.txtSub.Size = new System.Drawing.Size(100, 26);
+            this.txtSub.TabIndex = 14;
+            this.txtSub.Text = "0";
+            this.txtSub.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(356, 595);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(100, 26);
+            this.txtDiscount.TabIndex = 16;
+            this.txtDiscount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtDiscount.TextChanged += new System.EventHandler(this.txtDiscount_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label7.Location = new System.Drawing.Point(264, 598);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 22);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Discount:-";
+            // 
+            // txtNet
+            // 
+            this.txtNet.Location = new System.Drawing.Point(635, 598);
+            this.txtNet.Name = "txtNet";
+            this.txtNet.Size = new System.Drawing.Size(100, 26);
+            this.txtNet.TabIndex = 18;
+            this.txtNet.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label8.Location = new System.Drawing.Point(516, 601);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 22);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Net Amount:-";
+            // 
+            // txtPaid
+            // 
+            this.txtPaid.Location = new System.Drawing.Point(869, 598);
+            this.txtPaid.Name = "txtPaid";
+            this.txtPaid.Size = new System.Drawing.Size(100, 26);
+            this.txtPaid.TabIndex = 20;
+            this.txtPaid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPaid.TextChanged += new System.EventHandler(this.txtPaid_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label9.Location = new System.Drawing.Point(777, 601);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 22);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Paid:-";
+            // 
+            // txtBalance
+            // 
+            this.txtBalance.Location = new System.Drawing.Point(869, 667);
+            this.txtBalance.Name = "txtBalance";
+            this.txtBalance.Size = new System.Drawing.Size(100, 26);
+            this.txtBalance.TabIndex = 22;
+            this.txtBalance.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label10.Location = new System.Drawing.Point(777, 670);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 22);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Balance:-";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(240, 270);
+            this.txtQty.Multiline = true;
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 38);
+            this.txtQty.TabIndex = 23;
+            this.txtQty.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(706, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 64);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BillingSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 715);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtBalance);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtPaid);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtNet);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSub);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnAddtocart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.radioLadies);
+            this.Controls.Add(this.radioGents);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbItems);
+            this.Controls.Add(this.panel1);
+            this.Name = "BillingSystem";
+            this.Text = "BillingSystem";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbItems;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioGents;
+        private System.Windows.Forms.RadioButton radioLadies;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnAddtocart;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSub;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNet;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPaid;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBalance;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BillingInvoice/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillingInvoice
+{
+    public partial class BillingSystem : Form
+    {
+        public BillingSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void radioGents_CheckedChanged(object sender, EventArgs e)
+        {
+            radioGents.ForeColor = System.Drawing.Color.Green;
+            radioLadies.ForeColor = System.Drawing.Color.Red;
+
+            cmbItems.Items.Clear();
+            cmbItems.Items.Add("Shirts");
+            cmbItems.Items.Add("Pants");
+            cmbItems.Items.Add("T-Shirts");
+            cmbItems.Items.Add("Tracks");
+            cmbItems.Items.Add("Shorts");
+            cmbItems.Items.Add("Inners");
+
+        }
+
+        private void radioLadies_CheckedChanged(object sender, EventArgs e)
+        {
+            radioGents.ForeColor = System.Drawing.Color.Red;
+            radioLadies.ForeColor = System.Drawing.Color.Green;
+            cmbItems.Items.Clear();
+            cmbItems.Items.Add("Sarees");
+            cmbItems.Items.Add("Half Sarees");
+            cmbItems.Items.Add("Wedding Clothes");
+            cmbItems.Items.Add("Tops");
+            cmbItems.Items.Add("Dress");
+            cmbItems.Items.Add("Thong");
+
+        }
+
+        private void cmbItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbItems.SelectedItem.ToString()=="Shirts")
+            {
+                txtPrice.Text = "350";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Pants")
+            {
+                txtPrice.Text = "700";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "T-Shirts")
+            {
+                txtPrice.Text = "250";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Tracks")
+            {
+                txtPrice.Text = "300";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Shorts")
+            {
+                txtPrice.Text = "150";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Inners")
+            {
+                txtPrice.Text = "100";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Sarees")
+            {
+                txtPrice.Text = "500";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Half Sarees")
+            {
+                txtPrice.Text = "1500";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Wedding Clothes")
+            {
+                txtPrice.Text = "2500";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Tops")
+            {
+                txtPrice.Text = "300";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Thong")
+            {
+                txtPrice.Text = "150";
+            }
+            else if (cmbItems.SelectedItem.ToString() == "Shirts")
+            {
+                txtPrice.Text = "50";
+            }
+            else
+            {
+                txtPrice.Text = "0";
+            }
+            txtQty.Text = "";
+            txtTotal.Text = "";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtQty.Text.Length > 0)
+            {
+                txtTotal.Text = (Convert.ToInt32(txtPrice.Text) * Convert.ToInt32(txtQty.Text)).ToString();
+            }
+        }
+
+        private void btnAddtocart_Click(object sender, EventArgs e)
+        {
+            string[] arr = new string[8];
+            arr[0] = cmbItems.SelectedItem.ToString();
+            arr[1] = txtPrice.Text;
+            arr[2] = txtQty.Text;
+            arr[3] = txtTotal.Text;
+
+            ListViewItem lvi = new ListViewItem(arr);
+            listView1.Items.Add(lvi);
+
+            txtSub.Text = (Convert.ToInt32(txtSub.Text) + Convert.ToInt32(txtTotal.Text)).ToString();
+        }
+
+        private void txtDiscount_TextChanged(object sender, EventArgs e)
+        {
+            if(txtDiscount.Text.Length>0)
+            {
+                txtNet.Text = (Convert.ToInt32(txtSub.Text) - Convert.ToInt32(txtDiscount.Text)).ToString();
+            }
+        }
+
+        private void txtPaid_TextChanged(object sender, EventArgs e)
+        {
+            if(txtPaid.Text.Length>0)
+            {
+                txtBalance.Text = (Convert.ToInt32(txtNet.Text) - Convert.ToInt32(txtPaid.Text)).ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count>0)
+            {
+                for(int i=0;i<listView1.Items.Count;i++)
+                {
+                    if(listView1.Items[i].Selected)
+                    {
+                        txtSub.Text = (Convert.ToInt32(txtSub.Text) - Convert.ToInt32(listView1.Items[i].SubItems[3].Text)).ToString();
+                        listView1.Items[i].Remove();
+
+
+                    }
+                }
+            }
+        }
+    }
+}
